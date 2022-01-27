54e9dfba6f1245232da1b89bd22c3e30f6d9557d
Austin Mason <austinpmason@gmail.com>
2022-01-27 20:59:04 +0000

    Add tailwind stylings to sign in and edit user views

--- app/form_builders/tailwind_form_builder.rb
+++ app/form_builders/tailwind_form_builder.rb
@@ -1,14 +1,19 @@
 class TailwindFormBuilder < ActionView::Helpers::FormBuilder
+  # rubocop:disable Layout/LineLength
   ELEMENT_CLASS_MAPPINGS = {
-    input: 'appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm'
-  }
+    check_box: 'focus:ring-orange-500 h-4 w-4 text-orange-600 border-gray-300 rounded',
+    input: 'appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm',
+    label: 'block text-sm font-medium text-gray-700',
+    submit_button: 'w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-400 hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-300'
+  }.freeze
+  # rubocop:enable Layout/LineLength
 
   def label(method, text = nil, options = {}, &block)
     super(
       method,
       text,
       options.reverse_merge(
-        class: 'block text-sm font-medium text-gray-700'
+        class: ELEMENT_CLASS_MAPPINGS.fetch(:label)
       ),
       &block
     )
@@ -36,7 +41,7 @@ class TailwindFormBuilder < ActionView::Helpers::FormBuilder
     super(
       method,
       options.reverse_merge(
-        class: 'focus:ring-orange-500 h-4 w-4 text-orange-600 border-gray-300 rounded'
+        class: ELEMENT_CLASS_MAPPINGS.fetch(:check_box)
       ),
       checked_value,
       unchecked_value
@@ -47,7 +52,7 @@ class TailwindFormBuilder < ActionView::Helpers::FormBuilder
     super(
       value,
       options.reverse_merge(
-        class: 'w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-400 hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-300'
+        class: ELEMENT_CLASS_MAPPINGS.fetch(:submit_button)
       )
     )
   end
