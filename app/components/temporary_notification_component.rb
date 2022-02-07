3e7ab4f7daa93228627b16730e378c9191555c41
Austin Mason <austinpmason@gmail.com>
2022-02-07 14:21:24 +0000

    Create TemporaryNotification component

---
+++ app/components/temporary_notification_component.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class TemporaryNotificationComponent < ViewComponent::Base
+  def initialize(type:, message:)
+    @type = type
+    @message = message
+  end
+
+  def type_icon
+    {
+      alert: 'icons/x_circle_outline',
+      notice: 'icons/check_circle_outline'
+    }.fetch(@type.to_sym)
+  end
+
+  def type_class
+    {
+      alert: 'h-6 w-6 text-red-400',
+      notice: 'h-6 w-6 text-green-400'
+    }.fetch(@type.to_sym)
+  end
+end
