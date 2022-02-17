5db7039ded63f7aab12ff2ec70824d4095c7f055
Austin Mason <austinpmason@gmail.com>
2022-02-17 20:24:15 +0000

    Create Product admin dashboard
    Standard admin dashboard to add, edit or delete products
    
    - Create admin products controller
    - Create admin product dashboard
    - Updates how insurers are displayed on a page to its name
    - Adds the resource to the routes file for the admin product dashboard

--- app/dashboards/product_dashboard.rb
+++ app/dashboards/product_dashboard.rb
@@ -1,6 +1,6 @@
 require 'administrate/base_dashboard'
 
-class InsurerDashboard < Administrate::BaseDashboard
+class ProductDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
   # a hash that describes the type of each of the model's fields.
   #
@@ -8,6 +8,7 @@ class InsurerDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
+    insurer: Field::BelongsTo,
     id: Field::Number,
     name: Field::String,
     created_at: Field::DateTime,
@@ -20,15 +21,16 @@ class InsurerDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
+    insurer
     id
     name
     created_at
-    updated_at
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
+    insurer
     id
     name
     created_at
@@ -39,6 +41,7 @@ class InsurerDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
+    insurer
     name
   ].freeze
 
@@ -54,10 +57,10 @@ class InsurerDashboard < Administrate::BaseDashboard
   #   }.freeze
   COLLECTION_FILTERS = {}.freeze
 
-  # Overwrite this method to customize how insurers are displayed
+  # Overwrite this method to customize how products are displayed
   # across all pages of the admin dashboard.
 
-  def display_resource(insurer)
-    insurer.name
+  def display_resource(product)
+    product.name
   end
 end
