13a74b20b6806dc1ef00146ace49aa5f01de030f
Austin Mason <austinpmason@gmail.com>
2022-06-22 13:12:18 +0100

    remove setting capybara host

--- spec/support/better_rails_system_tests.rb
+++ spec/support/better_rails_system_tests.rb
@@ -17,15 +17,6 @@ end
 RSpec.configure do |config|
   config.include BetterRailsSystemTests, type: :system
 
-  # Make urls in mailers contain the correct server host.
-  # It's required for testing links in emails (e.g., via capybara-email).
-  config.around(:each, type: :system) do |ex|
-    was_host = Rails.application.default_url_options[:host]
-    Rails.application.default_url_options[:host] = Capybara.server_host
-    ex.run
-    Rails.application.default_url_options[:host] = was_host
-  end
-
   # Make sure this hook runs before others
   config.prepend_before(:each, type: :system) do
     # Use JS driver always
