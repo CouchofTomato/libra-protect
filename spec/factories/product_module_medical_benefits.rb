034b8d5573a1d8ec2b22e4fb4a6ca335320b1cff
Austin Mason <austinpmason@gmail.com>
2023-08-05 15:05:51 +0100

    Fix rubocop offences

--- spec/factories/product_module_medical_benefits.rb
+++ spec/factories/product_module_medical_benefits.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :product_module_medical_benefit do
-    association :product_module, :core_product_module
+    product_module factory: %i[product_module core_product_module]
     medical_benefit
     benefit_limit { "MyString" }
     benefit_limit_status { "paid_in_full" }
